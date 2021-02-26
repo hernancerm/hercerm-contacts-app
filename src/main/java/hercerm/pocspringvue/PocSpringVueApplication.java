89e838e48d56461b89ce3954334f68667e5762b5
Hernán Cervera <hjcerveram@hotmail.com>
2021-02-26 10:15:00 -0600

    Initial commit

---
+++ src/main/java/hercerm/pocspringvue/PocSpringVueApplication.java
@@ -0,0 +1,13 @@
+package hercerm.pocspringvue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocSpringVueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocSpringVueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hercerm/pocspringvue/PocSpringVueApplicationTests.java
@@ -0,0 +1,13 @@
+package hercerm.pocspringvue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocSpringVueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
