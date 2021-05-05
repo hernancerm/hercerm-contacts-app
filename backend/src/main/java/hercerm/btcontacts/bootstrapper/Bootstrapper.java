2bbf224bc22744f43d84d9b5bc68413b8ea9559d
Hernán Cervera <hjcerveram@hotmail.com>
2021-05-05 18:30:00 -0500

    Expand test data

--- backend/src/main/java/hercerm/btcontacts/bootstrapper/Bootstrapper.java
+++ backend/src/main/java/hercerm/btcontacts/bootstrapper/Bootstrapper.java
@@ -16,10 +16,33 @@ public class Bootstrapper implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        Contact hernan = new Contact("Hernan", "Cervera", "hernan@example.com");
-        Contact gladys = new Contact("Gladys", "Cervera", "gladys@example.com");
+        Contact hernan = new Contact("Hernán", "Cervera", "hernan@example.com");
+        Contact roman = new Contact("Roman", "Polanski", "roman@example.com");
+        Contact darren = new Contact("Darren", "Aronofsky", "darren@example.com");
+        Contact vincent = new Contact("Vincent", "Vega", "vincent@example.com");
+        Contact sigmund = new Contact("Sigmund", "Freud", "signumd@example.com");
+        Contact richard = new Contact("Richard", "Feynman", "richard@example.com");
+        Contact carol = new Contact("Carol", "Dweck", "carol@example.com");
+        Contact friedrich = new Contact("Friedrich", "Nietzsche", "friedrich@example.com");
+        Contact stanley = new Contact("Stanley", "Kubrick", "stanley@example.com");
+        Contact katniss = new Contact("Katniss", "Everdeen", "katsniss@example.com");
+        Contact marty = new Contact("Marty", "McFly", "marty@example.com");
+        Contact rick = new Contact("Rick", "Sanchez", "rick@example.com");
+        Contact erich = new Contact("Erich", "Fromm", "rick@example.com");
 
         contactRepository.save(hernan);
-        contactRepository.save(gladys);
+        contactRepository.save(roman);
+        contactRepository.save(darren);
+        contactRepository.save(vincent);
+        contactRepository.save(sigmund);
+        contactRepository.save(richard);
+        contactRepository.save(carol);
+        contactRepository.save(friedrich);
+        contactRepository.save(stanley);
+        contactRepository.save(katniss);
+        contactRepository.save(marty);
+        contactRepository.save(richard);
+        contactRepository.save(rick);
+        contactRepository.save(erich);
     }
 }
