31daefc7403a7e9768ac9e6625139eb08414d2bf
Hernán Cervera <hjcerveram@hotmail.com>
2021-07-03 15:07:10 -0500

    Improve /swagger-ui.html and OpenAPI endpoints
    - Add summary to endpoints.
    - Change controller name to "Contacts" in documentation.
    - Change open-api path to /openapi3.json
    - Add application top-level title and description.
    
    Additionally, document faced difficulties with springdoc.

--- backend/src/main/java/hercerm/contactsapp/BreakableToyContactsApps.java
+++ backend/src/main/java/hercerm/contactsapp/BreakableToyContactsApps.java
@@ -1,9 +1,26 @@
 package hercerm.contactsapp;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*
+ * SpringDoc documentation:
+ * https://springdoc.org/
+ *
+ * How to use SpringDoc:
+ * https://www.baeldung.com/spring-rest-openapi-documentation
+ *
+ * Swagger annotations:
+ * https://javadoc.io/doc/io.swagger.core.v3/swagger-annotations/latest/index.html
+ */
+
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(
+		title = "Contacts API",
+		description = "Provide data and operations to a small Java+SpringBoot+Vue app deployed in Heroku."
+))
 public class BreakableToyContactsApps {
 
 	public static void main(String[] args) {
--- backend/src/main/java/hercerm/contactsapp/domain/contact/ContactController.java
+++ backend/src/main/java/hercerm/contactsapp/domain/contact/ContactController.java
@@ -1,9 +1,13 @@
 package hercerm.contactsapp.domain.contact;
 
 import hercerm.contactsapp.errors.InvalidContactException;
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
@@ -12,17 +16,21 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
-// https://www.baeldung.com/spring-rest-openapi-documentation
-// TODO:
-//   -  Add description per endpoint.
-//   - Change name of controller in Swagger UI to "Contacts".
-//   - Change path /v3/api-docs to "/openapi.js".
+/*
+ * Problems I see with springdoc:
+ * - @Operation(responses = {...}) overrides the auto-generated success response.
+ *   This requires a manual entry of @ApiResponse, which has duplicate information
+ *   regarding the annotation @ResponseStatus and the return type of the method.
+ * - Pageable query params documentation does not work according to:
+ *   - https://springdoc.org/#spring-data-rest-support or
+ *   - https://www.baeldung.com/spring-rest-openapi-documentation#exposing-pagination-information
+ * - Setting the schema of an @ApiResponse is too verbose (nesting within @Content).
+ * - Map.class schema does not seem to work, it is registered as string.
+ */
 
 @RestController
 @RequestMapping(ContactController.BASE_URL)
-// Add content to the generated API docs:
-// https://springdoc.org/#adding-api-information-and-security-documentation
-@OpenAPIDefinition(info = @Info(title = "Contacts"))
+@Tag(name = "Contacts")
 public class ContactController {
 
     public static final String BASE_URL = "/api/contacts";
@@ -33,8 +41,8 @@ public class ContactController {
         this.contactService = contactService;
     }
 
-    @Operation(summary = "get contacts paginated")
     @GetMapping
+    @Operation(summary = "get contacts paginated")
     public Page<Contact> getContactsPaginated(
             @PageableDefault(size = 10) Pageable pageable,
             // By default request params are required. When optional, if not
@@ -44,17 +52,35 @@ public class ContactController {
     }
 
     @GetMapping("/{contactId}")
+    @Operation(summary = "get a contact by id", parameters = {
+            @Parameter( // This is how parameters are documented.
+                    name = "contactId",
+                    in = ParameterIn.PATH,
+                    required = true)
+    })
     public Contact findContactById(@PathVariable long contactId) {
         return contactService.findById(contactId);
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @Operation(
+            summary = "create a contact",
+            responses = { // Here are the possible responses documented.
+                    @ApiResponse(
+                            responseCode = "201",
+                            content = @Content(schema = @Schema(implementation = Contact.class))),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "Invalid contact",
+                            content = @Content(schema = @Schema(implementation = Map.class)))
+            })
     public Contact createContact(@RequestBody Contact contact) {
         return contactService.createContact(contact);
     }
 
     @PutMapping("/{contactId}")
+    @Operation(summary = "update a contact")
     public Contact updateContact(
             @RequestBody Contact contact, @PathVariable long contactId) {
         return contactService.updateContact(contact, contactId);
@@ -62,6 +88,7 @@ public class ContactController {
 
     @DeleteMapping("/{contactId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
+    @Operation(summary = "hard delete a contact")
     public void deleteContact(@PathVariable long contactId) {
         contactService.deleteContact(contactId);
     }
